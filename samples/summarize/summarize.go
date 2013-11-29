d9dbde1af3ad71302739c6fb70926b6c5735d607
Mateus Braga <mateus.a.braga@gmail.com>
2013-11-29 11:17:09 -0200

    Get throughput on the first line

--- samples/summarize/summarize.go
+++ samples/summarize/summarize.go
@@ -1,48 +1,56 @@
 package main
 
 import (
-    "os"
-    "strconv"
-    "bufio"
-    "log"
-    "fmt"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 
-    "github.com/mateusbraga/gostat"
+	"github.com/mateusbraga/gostat"
 )
 
 func main() {
-    if len(os.Args) <= 1 {
-        log.Fatalln("You need to pass the file to summarize")
-    }
+	if len(os.Args) <= 1 {
+		log.Fatalln("You need to pass the file to summarize")
+	}
 
-    for _, filename := range os.Args[1:] {
-        file, err := os.Open(filename)
-        if err != nil {
-            log.Fatalln("Error to read file:", err)
-        }
+	for _, filename := range os.Args[1:] {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalln("Error to read file:", err)
+		}
 
-        scanner := bufio.NewScanner(file)
+		scanner := bufio.NewScanner(file)
 
-        var data []int64
-        for scanner.Scan() {
-            val, err := strconv.ParseInt(scanner.Text(), 10, 0)
-            if err != nil {
-                log.Fatalln("error parsing int:", err)
-            }
-            data = append(data, val)
-        }
-        if err := scanner.Err(); err != nil {
-            log.Fatalln("error:", err)
-        }
+		var data []int64
+		var throughput float64
 
-        filteredData := gostat.TakeExtremes(data)
-        log.Println("size:", len(filteredData), filteredData)
-        mean := gostat.Mean(filteredData)
-        sd := gostat.StandardDeviation(filteredData)
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			log.Fatalln("error:", err)
+		}
 
-        fmt.Println(filename, ":", mean, sd)
-    }
+		throughput, err = strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			log.Fatalln("error parsing float:", err)
+		}
+
+		for scanner.Scan() {
+			val, err := strconv.ParseInt(scanner.Text(), 10, 0)
+			if err != nil {
+				log.Fatalln("error parsing int:", err)
+			}
+			data = append(data, val)
+		}
+		if err := scanner.Err(); err != nil {
+			log.Fatalln("error:", err)
+		}
+
+		filteredData := gostat.TakeExtremes(data)
+		mean := gostat.Mean(filteredData)
+		sd := gostat.StandardDeviation(filteredData)
+
+		fmt.Printf("%v: %v %v:%v\n", filename, mean, sd, throughput)
+	}
 }
-
-
-
